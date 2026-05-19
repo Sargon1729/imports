5e3da945791ff8eb6842b686d0c5fd5025d9759a
Sargon1729 <shadow-elephant@proton.me>
2026-05-19 20:02:24 +0100

    updated the main script which I keep forgetting

--- main.sh
+++ main.sh
@@ -1,6 +1,8 @@
-IMPORT="if [ -f $HOME/imports/bashrc-import.sh ]; then . $HOME/imports/bashrc-import.sh ; fi"
+IMPORT="if [ -f $HOME/imports/.bashrc-imports ]; then . $HOME/imports/.bashrc-imports ; fi"
 
 echo "#Bash Imports" >> $HOME/.bashrc
 echo "$IMPORT" >> $HOME/.bashrc
 
 source ~/.bashrc
+
+
